80f5d32f19e998e7534104b9dd4e2232405d03e7
DivyanshuRawatDev <divyanshu.rawat.dev@gmail.com>
2025-07-09 15:32:16 +0530

    Fix cors issue

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8080", {
+const socket = io("https://job-tracker-zulr.onrender.com", {
   transports: ["websocket"],
   withCredentials: true,
  
