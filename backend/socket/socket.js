4803c8d0f2cf2b1d8161d636f5203a740d7b16ec
DivyanshuRawatDev <divyanshu.rawat.dev@gmail.com>
2025-07-09 15:25:55 +0530

    Fix cors issue

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -10,7 +10,7 @@ const initilizedSocket = (server) => {
       credentials: true,
       origin: [
         "http://localhost:5173",
-        "https://job-tracker-zulr.onrender.com"
+        "https://job-tracker-xlbc.vercel.app/"
       ],
     },
   });
